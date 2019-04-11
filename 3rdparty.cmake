141267332b5b5db242435bc38dbd1024a049e04d
linmin <linmin093@gmail.com>
2019-04-11 17:54:40 +0800

    加入easylogger，修改cmake脚本

--- 3rdparty.cmake
+++ 3rdparty.cmake
@@ -1,4 +1,16 @@
 include(ExternalProject)
+
+set(USE_FAST_COMPLIE true)
+set(USE_CROSS_COMPLIE true)
+
+if(USE_CROSS_COMPLIE)
+set(CROSS_COMPLIE_OPTION -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_C_COMPILER=arm-hisiv500-linux-gcc -DCMAKE_CXX_COMPILER=arm-hisiv500-linux-g++)
+endif()
+
+if(USE_FAST_COMPLIE)
+set(FAST_COMPLIE_OPTION -j4)
+endif()
+
 #libav
 ExternalProject_Add(libav
     URL https://github.com/lam2003/monitor_3rdparty/raw/master/libav-12.3.tar.gz
@@ -6,7 +18,7 @@ ExternalProject_Add(libav
     SOURCE_DIR libav
     CONFIGURE_COMMAND     cd ${PROJECT_BINARY_DIR}/libav && ./configure --arch=arm  --cross-prefix=arm-hisiv500-linux- --enable-static --disable-shared  --enable-cross-compile --target_os=linux --prefix=${PROJECT_BINARY_DIR}/libav/build
     BUILD_IN_SOURCE 1
-    BUILD_COMMAND make
+    BUILD_COMMAND make ${FAST_COMPLIE_OPTION}
     INSTALL_COMMAND make install && cp ${PROJECT_BINARY_DIR}/libav/build/include ${PROJECT_BINARY_DIR} -rf && cp ${PROJECT_BINARY_DIR}/libav/build/lib ${PROJECT_BINARY_DIR} -rf
     )
 #jsoncpp
@@ -14,8 +26,18 @@ ExternalProject_Add(jsoncpp
     URL https://github.com/lam2003/monitor_3rdparty/raw/master/jsoncpp-0.10.7.zip
     DOWNLOAD_NAME jsoncpp-0.10.7.zip
     SOURCE_DIR jsoncpp
-    CONFIGURE_COMMAND cd ${PROJECT_BINARY_DIR}/jsoncpp && cmake . -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_C_COMPILER=arm-hisiv500-linux-gcc -DCMAKE_CXX_COMPILER=arm-hisiv500-linux-g++ -DJSONCPP_WITH_TESTS=OFF -DJSONCPP_WITH_POST_BUILD_UNITTEST=OFF -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/jsoncpp/build
+    CONFIGURE_COMMAND cd ${PROJECT_BINARY_DIR}/jsoncpp && cmake . ${CROSS_COMPLIE_OPTION} -DJSONCPP_WITH_TESTS=OFF -DJSONCPP_WITH_POST_BUILD_UNITTEST=OFF -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/jsoncpp/build
     BUILD_IN_SOURCE 1
-    BUILD_COMMAND make
-    INSTALL_COMMAND make install && cp ${PROJECT_BINARY_DIR}/jsoncpp/build/include ${PROJECT_BINARY_DIR} -rf && cp ${PROJECT_BINARY_DIR}/jsoncpp/build/lib ${PROJECT_BINARY_DIR}/lib/ -rf
+    BUILD_COMMAND make ${FAST_COMPLIE_OPTION}
+    INSTALL_COMMAND make install && cp ${PROJECT_BINARY_DIR}/jsoncpp/build/include ${PROJECT_BINARY_DIR} -rf && cp ${PROJECT_BINARY_DIR}/jsoncpp/build/lib ${PROJECT_BINARY_DIR} -rf
+    )
+#easyloggrt
+ExternalProject_Add(EasyLogger
+    URL https://github.com/lam2003/monitor_3rdparty/raw/master/EasyLogger.tar.gz
+    DOWNLOAD_NAME EasyLogger.tar.gz
+    SOURCE_DIR EasyLogger
+    CONFIGURE_COMMAND cd ${PROJECT_BINARY_DIR}/EasyLogger && cmake . ${CROSS_COMPLIE_OPTION} -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/EasyLogger/build
+    BUILD_IN_SOURCE 1
+    BUILD_COMMAND make ${FAST_COMPLIE_OPTION}
+    INSTALL_COMMAND make install && cp ${PROJECT_BINARY_DIR}/EasyLogger/build/include ${PROJECT_BINARY_DIR} -rf && cp ${PROJECT_BINARY_DIR}/EasyLogger/build/lib ${PROJECT_BINARY_DIR} -rf
     )
